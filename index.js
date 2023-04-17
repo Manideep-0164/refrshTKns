3a6c23c03c05cb9f9d2ac3eba969850a3ef6f54a
Manideep-0164 <peddaboinimanideep03@gmail.com>
2023-04-17 16:56:10 +0530

    data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { connection } = require('./configs/db');
-const { userRouter, blogRouter } = require('./routes/user.route');
+const { userRouter } = require('./routes/user.route');
+const { blogRouter } = require('./routes/blog.route');
 const { authentication } = require('./middlewares/authentication.middleware');
 const app = express();
 require('dotenv').config();
